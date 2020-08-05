920bab73c4ba492e267bfecca5765ae8877097f4
Enrique Ruiz <dundil@gmail.com>
2020-08-05 14:27:27 +0200

    Added new modules for CLI. Added init command and help.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const clui = require('clui');
+const Spinner = clui.Spinner;
+
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 var velocity = require("./alira");
-
-var port = '8888';
 
 let currentArgument = '';
 let arguments = process.argv.slice(2);
@@ -15,39 +18,184 @@ if (arguments.length == 0){
 }else{
 	let command = arguments[0];
 	switch (command){
+		case 'init':
+			initProject(arguments.slice(1));
+			break;
+		case 'update':
+			break;
+		case 'deploy':
+			break;
 		case 'serve':
 			initServer(arguments.slice(1));
 			break;
+		case 'help':
+			printHelp();
+			break;
 		default:
-			console.log('"ERROR: Command unknown');
+			console.log('"ERROR: Command unknown. Please try with these commands:');
+			printHelp();
 			return;
 	}
 }
 
-function initServer(args){
+async function initProject(args){
+	let gms = '';
+	let isHelp = false;
 	args.forEach(function (val, index, array) {
-		if (currentArgument == 'port'){
+		if (currentArgument){
+			switch (currentArgument){
+				case 'gms':
+					gms = val;
+					break;
+			}
 			currentArgument = '';
-			port = val;
-		}
-		if (val == '--port'){
-			currentArgument = 'port'
+		}else{
+			switch (val){
+				case '--gms':
+					currentArgument = 'gms';
+					break;
+				case '--help':
+					isHelp = true;
+					printInitHelp();
+					break;
+			}
 		}
 	});
-	var handler = {};
-	handler["SERVER"] = requestHandlers.serverHandler;
-	handler["ROOT"] = requestHandlers.rootHandler;
-	handler["html"] = requestHandlers.webPageHandler;
-	handler["jpg"] = requestHandlers.imageHandler;
-	handler["jpeg"] = requestHandlers.imageHandler;
-	handler["png"] = requestHandlers.imageHandler;
-	handler["gif"] = requestHandlers.imageHandler;
-	handler["svg"] = requestHandlers.svgHandler;
-	handler["json"] = requestHandlers.textFileHandler;
-	handler["js"] = requestHandlers.textFileHandler;
-	handler["css"] = requestHandlers.textFileHandler;
-	handler["scss"] = requestHandlers.textFileHandler;
-	handler["ttf"] = requestHandlers.fontHandler;
+	if (!isHelp){
+		let projectData;
+		try{
+			projectData = fs.readFileSync('./project.json', {encoding:'utf8', flag:'r'});
+		}catch(e){
+			
+		}
+		if (projectData){
+			console.log("ERROR: Project already created.");
+		}else{
+			printLogo();
+			console.log("Welcome to Alira CLI. Initializing project...");
+			if (gms == ''){
+				gms = await inquirer.prompt([
+					{
+						name: 'gms',
+						type: 'input',
+						message: 'Please, enter GMS url:',
+						validate: function( value ) {
+							if (value.length) {
+								return value;
+							} else {
+								return 'Please enter GMS url:';
+							}
+						}
+					}
+				]);
+				let spinner = new Spinner("Connecting to GMS...");
+				spinner.start();
+				//await fetch(gms);
+				wait(5000);
+				spinner.stop();
+			}
+		}
+	}
+}
+
+function initServer(args){
+	let port = '8888';
+	let isHelp = false;
+	args.forEach(function (val, index, array) {
+		if (currentArgument){
+			switch (currentArgument){
+				case 'port':
+					port = val;
+					break;
+			}
+			currentArgument = '';
+		}else{
+			switch (val){
+				case '--port':
+					currentArgument = 'port';
+					break;
+				case '--help':
+					isHelp = true;
+					printServeHelp();
+					break;
+			}
+		}
+	});
+	if (!isHelp){
+		var handler = {};
+		handler["SERVER"] = requestHandlers.serverHandler;
+		handler["ROOT"] = requestHandlers.rootHandler;
+		handler["html"] = requestHandlers.webPageHandler;
+		handler["jpg"] = requestHandlers.imageHandler;
+		handler["jpeg"] = requestHandlers.imageHandler;
+		handler["png"] = requestHandlers.imageHandler;
+		handler["gif"] = requestHandlers.imageHandler;
+		handler["svg"] = requestHandlers.svgHandler;
+		handler["json"] = requestHandlers.textFileHandler;
+		handler["js"] = requestHandlers.textFileHandler;
+		handler["css"] = requestHandlers.textFileHandler;
+		handler["scss"] = requestHandlers.textFileHandler;
+		handler["ttf"] = requestHandlers.fontHandler;
+		
+		server.startServer(port, router.route, handler, velocity);
+	}
+}
+
+function printHelp(){
+	console.log(
+	`
+	usage: alira <command> [<options>]
+
+	Commands:
+		init			creates a new project in current folder
+		update			updates local files with Alira's database
+		deploy			updates Alira's database with local files
+		serve			starts a local server
+
+	To get command options help: alira <command> --help
+	`
+	);
+}
+
+function printInitHelp(){
+	console.log(
+		`
+	usage: alira init [<options>]
+
+	Options:
+		--gms <url>			provides GMS url.
+	`
+	);
+}
+
+function printServeHelp(){
+	console.log(
+		`
+	usage: alira serve [<options>]
+
+	Options:
+		--port <port>			sets the port value
+	`
+	);
+}
+
+function printLogo(){
+	console.log(
+		`
+          _ _
+    /\\   | (_)          
+   /  \\  | |_ _ __ __ _ 
+  / /\\ \\ | | | '__/ _  |
+ / ____ \\| | | | | (_| |
+/_/    \\_\\_|_|_|  \\__,_|
 	
-	server.startServer(port, router.route, handler, velocity);
+		`);
+}
+
+async function wait(ms){
+	var start = new Date().getTime();
+	var end = start;
+	while (end < start + ms) {
+		end = new Date().getTime();
+	}
 }
